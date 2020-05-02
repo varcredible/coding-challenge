903afb9d299632fed38df3ecdcccc5526b0da1bb
Stepan Zorin <stz.hom@gmail.com>
2020-05-03 01:33:29 +0300

    Update Source.cpp

--- C++/Math/Find the element that appears once/Source.cpp
+++ C++/Math/Find the element that appears once/Source.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 
-int findTheElement(int _arr[], const int _ARRAY_LENGTH)
-{
-	int ones = 0;
-	
-	for (size_t i = 0; i < _ARRAY_LENGHT; i++)
-	{
-		int mismatches = 0; // must be equal to the ARRAY_LENGHT
-		
-		for (size_t j = 0; j < _ARRAY_LENGHT; j++)
-		{
-			if (_arr[i] != _arr[j])
-			{
-				mismatches++;				
-				
-				if (mismatches + 1 == _ARRAY_LENGHT)
-				{
-					ones = _arr[i];
-				}
-			}
-		}
-	}
-
-	return ones;
-}
+int findTheElement(int _arr[], const int _ARRAY_LENGTH);
 
 int main()
 {
@@ -33,4 +10,29 @@ int main()
 	std::cout << findTheElement(arr, ARRAY_LENGHT) << std::endl; // -> Output: 2
 
 	return 0;
+}
+
+int findTheElement(int _arr[], const int _ARRAY_LENGTH)
+{
+	int ones = 0;
+
+	for (size_t i = 0; i < _ARRAY_LENGTH; i++)
+	{
+		int mismatches = 0; // must be equal to the ARRAY_LENGHT
+
+		for (size_t j = 0; j < _ARRAY_LENGTH; j++)
+		{
+			if (_arr[i] != _arr[j])
+			{
+				mismatches++;
+
+				if (mismatches + 1 == _ARRAY_LENGTH)
+				{
+					ones = _arr[i];
+				}
+			}
+		}
+	}
+
+	return ones;
 }
