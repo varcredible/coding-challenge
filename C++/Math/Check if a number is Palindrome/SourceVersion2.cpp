bbe4368fc7e1727e4fa9130f992f298175ff4b34
Stepan Zorin <stz.hom@gmail.com>
2020-05-03 01:13:58 +0300

    Update SourceVersion2.cpp
    the last improvements

--- C++/Math/Check if a number is Palindrome/SourceVersion2.cpp
+++ C++/Math/Check if a number is Palindrome/SourceVersion2.cpp
@@ -3,17 +3,19 @@
 #include <iostream>
 #include <string>
 
-bool isPalindrome(const int _N) // _n == 76667
-{
+bool isPalindrome(const int _N);
+
+int main() {
+	isPalindrome(76667) ? printf("Yes") : printf("No"); // -> Output: Yes
+	return 0;
+}
+
+bool isPalindrome(const int _N) {
 	std::string number = std::to_string(_N);
 	std::string reverse_number = number;
 	std::reverse(reverse_number.begin(), reverse_number.end());
 
-	return (number == reverse_number) ? true : false; // 76667 == 76667 -> true
+	return (number == reverse_number) ? true : false; 
 }
 
-int main()
-{
-	isPalindrome(76667) ? printf("Yes") : printf("No"); // -> Output: Yes
-	return 0;
-}
+
