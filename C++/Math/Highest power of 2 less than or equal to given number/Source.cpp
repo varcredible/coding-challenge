02f06471b6977b55b621afdeabe898761ffe00f7
Stepan Zorin <stz.hom@gmail.com>
2020-05-03 01:35:02 +0300

    Update Source.cpp

--- C++/Math/Highest power of 2 less than or equal to given number/Source.cpp
+++ C++/Math/Highest power of 2 less than or equal to given number/Source.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
-int findHighestPowerOfTwo(const int _N)
-{
-	int HighestPower = 0;
-	
-	for (size_t powResult = 1; powResult <= _N; powResult *= 2)
-	{
-		HighestPower = powResult;
-	}
-	
-	return HighestPower;
-}
+int findTheHighestPowerOfTwo(const int _N);
 
 int main()
 {
 	std::cout << findTheHighestPowerOfTwo(10); // -> Output: 8
-	
+
 	return 0;
+}
+
+int findTheHighestPowerOfTwo(const int _N)
+{
+	int HighestPower = 0;
+
+	for (size_t powResult = 1; powResult <= _N; powResult *= 2)
+	{
+		HighestPower = powResult;
+	}
+
+	return HighestPower;
 }
