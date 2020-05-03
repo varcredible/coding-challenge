b2d50e0c5c1a39cc0ccd8fb974435804718a48af
Stepan Zorin <stz.hom@gmail.com>
2020-05-03 03:58:10 +0300

    Update Source.cpp

--- C++/Math/Find subarray with given sum. Set 1 (Nonnegative Numbers)/Source.cpp
+++ C++/Math/Find subarray with given sum. Set 1 (Nonnegative Numbers)/Source.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 
-int findSubarray(int _arr[], const unsigned int _ARRAY_LENGTH, int _sum);
+int findSubarray(int _arr[], const unsigned int _ARRAY_LENGTH, const int _SUM);
 
 int main()
 {
 	int arr[] = { 1, 4, 0, 0, 3, 10, 5 };
 	const unsigned int ARRAY_LENGTH = sizeof(arr) / sizeof(int);
 	int sum = 7;
-	findSubarray(arr, ARRAY_LENGTH, sum);
+	findSubarray(arr, ARRAY_LENGTH, sum); // -> Output: Sum found between indexes 1 and 4
 }
 
-int findSubarray(int _arr[], const unsigned int _ARRAY_LENGTH, int _sum)
+int findSubarray(int _arr[], const unsigned int _ARRAY_LENGTH, const int _SUM)
 {
 	int sumOfTheElements = 0;
 	int nextIndexNumber = 0;
 
 	for (size_t indexNumber = 0; indexNumber < _ARRAY_LENGTH; indexNumber++)
 	{
-		if (sumOfTheElements < _sum || _sum == 0)
+		if (sumOfTheElements < _SUM || _SUM == 0)
 		{
 			sumOfTheElements += _arr[indexNumber];
 		}
 
-		else if(sumOfTheElements == _sum)
+		else if(sumOfTheElements == _SUM)
 		{
 			printf("Sum found between indexes %d and %d", nextIndexNumber, indexNumber - 1);
 			return 0;
