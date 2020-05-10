981c32560d3a6c23c6dc2d7059fa78634722555f
Stepan Zorin <stz.hom@gmail.com>
2020-05-10 16:49:15 +0300

    Update Source.cpp

--- C++/Math/Check if a number is Palindrome/Source.cpp
+++ C++/Math/Check if a number is Palindrome/Source.cpp
@@ -1,9 +1,16 @@
+/* 
+
+This code only works with four-digit numbers. 
+If you're interested in solving any-digit numbers then check the other versions out in the current repository.
+
+*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    int n = 0;
-    std::cin >> n;
-
-    std::cout << (n / 100 - ((n % 10 * 10) + (n % 100 / 10))) + 1 << std::endl;
+    int n = 2002;
+    (((n / 100 - ((n % 10 * 10) + (n % 100 / 10))) + 1) == 1) ? cout << "YES" : cout << "NO"; // -> Output: YES
+    
     return 0;
 }
