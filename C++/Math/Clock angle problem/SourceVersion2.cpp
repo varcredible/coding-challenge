a5ab94db8e232e4f095fee224d828bc0b70f14cd
Stepan Zorin <stz.hom@gmail.com>
2020-05-10 17:08:31 +0300

    Update SourceVersion2.cpp

--- C++/Math/Clock angle problem/SourceVersion2.cpp
+++ C++/Math/Clock angle problem/SourceVersion2.cpp
@@ -2,10 +2,11 @@
 // The hour hand is moves regarding of the minute hand //
 
 #include <iostream>
+using namespace std;
 
 int getHoursAndMinutesFromUser(int* _ptr_hours, int* _ptr_minutes);
 int calculateAndShowTheDegreesBetweenHands(const int* _PTR_HOURS, const int* _PTR_MINUTES);
-int showMessageError(const int _ERRORNUMBER);
+int showErrorMessage(const int _ERRORNUMBER);
 
 int main()
 {
@@ -23,10 +24,10 @@ int getHoursAndMinutesFromUser(int* _ptr_hours, int* _ptr_minutes)
 
 	do
 	{
-		std::cout << "Enter hours: ";
-		std::cin >> *_ptr_hours;
-		std::cout << "Enter minutes: ";
-		std::cin >> *_ptr_minutes;
+		cout << "Enter hours: ";
+		cin >> *_ptr_hours;
+		cout << "Enter minutes: ";
+		cin >> *_ptr_minutes;
 
 		if (!std::cin.fail())
 		{
@@ -42,15 +43,15 @@ int getHoursAndMinutesFromUser(int* _ptr_hours, int* _ptr_minutes)
 
 			else
 			{
-				showMessageError(2);
+				showErrorMessage(2);
 			}
 		}
 
 		else
 		{
-			std::cin.clear();
-			std::cin.ignore(32767, '\n');
-			showMessageError(1);
+			cin.clear();
+			cin.ignore(32767, '\n');
+			showErrorMessage(1);
 		}
 
 	} while (!isCorrectData);
@@ -64,21 +65,21 @@ int calculateAndShowTheDegreesBetweenHands(const int* _PTR_HOURS, const int* _PT
 	double hoursDegrees = (minutesDegrees / 6) * ((2.4 / 5) + 1.02);
 	double theDegreesBetweenHands = abs(hoursDegrees - minutesDegrees);
 
-	std::cout << theDegreesBetweenHands << " degrees";
+	cout << theDegreesBetweenHands << " degrees";
 
 	return 0;
 }
 
-int showMessageError(const int _ERROR_NUMBER)
+int showErrorMessage(const int _ERROR_NUMBER)
 {
 	switch (_ERROR_NUMBER)
 	{
-	case 1: std::cout << "\nWrong input format! Only numbers! Try again" << std::endl;
+	case 1: cout << "\nWrong input format! Only numbers! Try again" << endl;
 		break;
-	case 2: std::cout << "\nValue of hours can't be over than \"24\" and value of minutes can't be over than \"60\". Try again." << std::endl;
+	case 2: cout << "\nValue of hours can't be over than \"24\" and value of minutes can't be over than \"60\". Try again." << endl;
 		break;
 
-	default: std::cout << "Unknown error!" << std::endl;
+	default: cout << "Unknown error!" << endl;
 		break;
 	}
 
