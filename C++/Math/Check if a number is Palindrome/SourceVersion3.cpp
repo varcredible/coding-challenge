8af4123f1710e33015929e9bcf50ada1b7cf76d4
Stepan Zorin <stz.hom@gmail.com>
2020-05-10 16:51:23 +0300

    Update and rename SourceVersion2.cpp to SourceVersion3.cpp

---
+++ C++/Math/Check if a number is Palindrome/SourceVersion3.cpp
@@ -0,0 +1,22 @@
+// A method using strings // 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(const int _N);
+
+int main() {
+	isPalindrome(76667) ? printf("Yes") : printf("No"); // -> Output: Yes
+	return 0;
+}
+
+bool isPalindrome(const int _N) {
+	string number = std::to_string(_N);
+	string reverse_number = number;
+	reverse(reverse_number.begin(), reverse_number.end());
+
+	return (number == reverse_number) ? true : false;
+}
+
+
