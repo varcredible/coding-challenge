1316a302300c85a279cd3523c563757556ddee96
Stepan Zorin <stz.hom@gmail.com>
2020-05-10 17:01:34 +0300

    Update Source.cpp

--- C++/Math/Check if a number is Palindrome/Source.cpp
+++ C++/Math/Check if a number is Palindrome/Source.cpp
@@ -8,7 +8,8 @@ If you're interested in solving any-digit numbers then check the other versions 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int n = 2002;
     (((n / 100 - ((n % 10 * 10) + (n % 100 / 10))) + 1) == 1) ? cout << "YES" : cout << "NO"; // -> Output: YES
     
